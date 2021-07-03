88c2142b344c42d6b514425eee2dda7c8bb349eb
Theresa <49374955+teiResa@users.noreply.github.com>
2021-07-03 19:12:02 +0800

    Create fr-couleurDefaut.ino

---
+++ fr-couleurDefaut.ino
@@ -0,0 +1,52 @@
+/*
+*	fr-couleurDefaut.ino		-	l’utilisation de la LedRGB Library
+* Ce programme aperçu les couleurs “couleurDefaut” fonction.
+*
+* Attention!	 RGB == RVB, ils sont les memes. Je préfère écrire « RGB ».
+*
+*
+*Par: Alejandro Bertinelli
+*Traduction: Theresa (teiResa)
+*
+*Création:	22 Juin 2019
+*Mise:		3 Juillet 2021
+*/
+
+/*
+* La licence:  MIT License 3.0
+*/
+
+#include <LedRGB.h>
+//Le programme est désigné pour la cathode commune ('common cathode') RGB LEDs. 
+//Si vous voulez utiliser une anode commune ('common anode'), replacer CC avec AA ici :
+LedRGB maLED(9, 10, 11, CC);
+
+void setup() {
+
+}
+
+void loop() {
+	//En utiliser toutes les couleurs libres par defaut 
+maLED.defautCouleur(“blanc”);
+delay(300);
+maLED.defautCouleur(“rouge”);
+delay(300);
+maLED.defautCouleur(“verte”);		<!-- or vert. I’ll check.
+delay(300);
+maLED.defautCouleur(“bleu”);
+delay(300);
+maLED.defautCouleur(“jeune”);
+delay(300);
+maLED.defautCouleur(“orange”);
+delay(300);
+maLED.defautCouleur(“mauve”);
+delay(300);
+maLED.defautCouleur(“turquoise”);		
+delay(300);
+maLED.defautCouleur(“gris”);
+delay(300);
+maLED.defautCouleur(“marron”);
+delay(300);
+maLED.defautCouleur(“fuchsia”);		//mauve-rose
+delay(300);
+}
